a1186dd2fd6aeb2f16ed21b18ceb43a9dfda6c94
hpnrep6 <hpnrep6@gmail.com>
2021-05-28 10:48:52 -0400

    batch-render-class

--- z0/graphics/renderbatch.js
+++ z0/graphics/renderbatch.js
@@ -1,8 +1,33 @@
+import { Sprite2DRenderer } from './sprite2d.js';
 
 export class RenderBatch {
+    static defaultVertCoords = [
+        1,  1,
+        1, -1,
+       -1,  1,
+        
+       -1,  1,
+        1, -1,
+       -1, -1
+    ];
+
+    
     sprites = [];
     texture;
     shader;
+    vao;
+    buffers = {
+        transform: null, // Location, rotation
+        size: null, // Scaling, alpha
+        texCoords: null // Texture coordinates
+    };
+
+    arrays = {
+        transform: [],
+        size: [],
+        texCoords: []
+    }
+    static vertCoords = [];
 
     updateLoc = false;
     updateSize= false;
@@ -14,9 +39,51 @@ export class RenderBatch {
         this.texture = texture;
     }
 
+    updateBuffers(gl) {
+        if(updateGeo) {
+            for(let i = 0; i < this.sprites.length; i++) {
+                let offset = i * 18;
+                {
+                    // Transformation buffers;
+                    // 3 per vertex, 6 vertices per quad
+
+                }
+
+                offset = i * 12;
+                {
+                    // Texture coordinate buffers
+                    // 2 per vertex, 6 vertices per quad
+                    
+                }
+            }
+
+        } else {
+
+        if(updateLoc) {
+
+        }
+
+        if(updateSize || updateAlpha) {
+
+        }
+
+        if(updateTexture) {
+
+        }
+
+        }
+
+
+        this.updateLoc = false;
+        this.updateSize= false;
+        this.updateTexture = false;
+        this.updateAlpha = false;
+        this.updateGeo = false;
+    }
+
     add(sprite) {
         this.sprites.push(sprite);
         sprite._batch = this;
-        console.log(sprite)
+        console.log(this.texture)
     }
 }
